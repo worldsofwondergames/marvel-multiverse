96279281951e6108efaf8dab9bee4172cae57873
Jeff Barnes <codemonkey1972@gmail.com>
2026-07-04 09:39:00 -0400

    Register headquarters actor/item types, add localization and pack entries (#69)

--- marvel-multiverse.mjs
+++ marvel-multiverse.mjs
@@ -466,7 +466,9 @@ const ITEM_DEFAULT_ICONS = {
   origin: "systems/marvel-multiverse/icons/origin.svg",
   powerSet: "icons/svg/card-hand.svg",
   power: "systems/marvel-multiverse/icons/super-powers.svg",
-  tag: "systems/marvel-multiverse/icons/tags.svg"
+  tag: "systems/marvel-multiverse/icons/tags.svg",
+  hqTag: "systems/marvel-multiverse/icons/tags.svg",
+  hqTrait: "systems/marvel-multiverse/icons/trait.svg"
 };
 
 let MarvelMultiverseItem$1 = class MarvelMultiverseItem extends Item {
@@ -5073,6 +5075,53 @@ class MarvelMultiverseVehicle extends foundry.abstract.TypeDataModel {
   }
 }
 
+class MarvelMultiverseHeadquarters extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const schema = {};
+
+    schema.health = new fields.SchemaField({
+      value: new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0 }),
+      max: new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0 }),
+    });
+
+    schema.members = new fields.ArrayField(new fields.SchemaField({
+      actorId: new fields.StringField({ required: true, blank: false }),
+      name: new fields.StringField({ required: true, blank: true }),
+      img: new fields.StringField({ required: true, blank: true }),
+    }));
+
+    schema.description = new fields.StringField({ required: true, blank: true });
+    schema.notes = new fields.StringField({ required: true, blank: true });
+    schema.source = new fields.StringField({ required: true, blank: true });
+
+    return schema;
+  }
+
+  prepareDerivedData() {
+    const hqTraits = this.parent?.items?.filter(i => i.type === "hqTrait") ?? [];
+    this.traitCount = hqTraits.length;
+    this.health.max = this.traitCount * 2;
+
+    const ranks = this.members
+      .map(m => game.actors?.get(m.actorId)?.system?.attributes?.rank?.value)
+      .filter(r => r != null)
+      .sort((a, b) => b - a)
+      .slice(0, 6);
+
+    this.teamRank = ranks.length > 0 ? Math.ceil(ranks.reduce((s, r) => s + r, 0) / ranks.length) : 1;
+    this.traitSlots = this.teamRank * 3;
+
+    this.health.damaged = this.health.max > 0 && this.health.value > 0 && this.health.value <= this.health.max / 2;
+    this.health.destroyed = this.health.max > 0 && this.health.value <= 0;
+
+    let healthStatus = "operational";
+    if (this.health.destroyed) healthStatus = "destroyed";
+    else if (this.health.damaged) healthStatus = "damaged";
+    this.health.status = healthStatus;
+  }
+}
+
 class MarvelMultiverseItemBase extends foundry.abstract.TypeDataModel {
 
   static defineSchema() {
@@ -5293,6 +5342,31 @@ class MarvelMultiverseTrait extends MarvelMultiverseItemBase {
 
         return schema;
     }
+}
+
+class MarvelMultiverseHqTag extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const schema = {};
+
+    schema.description = new fields.StringField({ required: true, blank: true });
+    schema.incompatible = new fields.StringField({ required: true, blank: true });
+
+    return schema;
+  }
+}
+
+class MarvelMultiverseHqTrait extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const schema = {};
+
+    schema.description = new fields.StringField({ required: true, blank: true });
+    schema.downtimeActivity = new fields.StringField({ required: true, blank: true });
+    schema.maxCount = new fields.NumberField({ required: true, nullable: false, integer: true, initial: 0, min: 0 });
+
+    return schema;
+  }
 }
 
 class MarvelMultiverseRestriction extends MarvelMultiverseItemBase {
@@ -6083,6 +6157,7 @@ Hooks.once("init", () => {
     character: MarvelMultiverseCharacter,
     npc: MarvelMultiverseNPC,
     vehicle: MarvelMultiverseVehicle,
+    headquarters: MarvelMultiverseHeadquarters,
   };
   CONFIG.ChatMessage.documentClass = ChatMessageMarvel;
   CONFIG.Item.documentClass = MarvelMultiverseItem$1;
@@ -6100,6 +6175,8 @@ Hooks.once("init", () => {
     battleSuit: MarvelMultiverseBattleSuit,
     equipment: MarvelMultiverseEquipment,
     vehicleWeapon: MarvelMultiverseVehicleWeapon,
+    hqTag: MarvelMultiverseHqTag,
+    hqTrait: MarvelMultiverseHqTrait,
   };
 
   game.settings.register("marvel-multiverse", "autoPopulateOrigin", {
@@ -6203,6 +6280,11 @@ Hooks.once("init", () => {
     types: ["vehicle"],
     makeDefault: true,
     label: "MARVEL_MULTIVERSE.SheetLabels.Vehicle",
+  });
+  Actors.registerSheet("marvel-multiverse", MarvelMultiverseHeadquartersSheet, {
+    types: ["headquarters"],
+    makeDefault: true,
+    label: "MARVEL_MULTIVERSE.SheetLabels.Headquarters",
   });
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("marvel-multiverse", MarvelMultiverseItemSheet, {
